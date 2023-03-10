2a97fddb1988a2a543ea4bdc1156216ae57adda7
Saurav <sp.gharti@gmail.com>
2023-03-10 15:29:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-limit-9",
+  url: "https://lively-limit-9.botics.co",
+  api: "https://lively-limit-9.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
